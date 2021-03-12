4c33d0d9c87ea7fff0621d14ec0f6e58a319a902
@vishnu.sri <vishnu.sri@zibtek.in>
2021-03-12 20:16:55 +0530

    check789654

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,77 @@
 // code from unbounce
+var module = {
+    options: [],
+    header: [navigator.platform, navigator.userAgent, navigator.appVersion, navigator.vendor, window.opera],
+    dataos: [
+        { name: 'Windows Phone', value: 'Windows Phone', version: 'OS' },
+        { name: 'Windows', value: 'Win', version: 'NT' },
+        { name: 'iPhone', value: 'iPhone', version: 'OS' },
+        { name: 'iPad', value: 'iPad', version: 'OS' },
+        { name: 'Kindle', value: 'Silk', version: 'Silk' },
+        { name: 'Android', value: 'Android', version: 'Android' },
+        { name: 'PlayBook', value: 'PlayBook', version: 'OS' },
+        { name: 'BlackBerry', value: 'BlackBerry', version: '/' },
+        { name: 'Macintosh', value: 'Mac', version: 'OS X' },
+        { name: 'Linux', value: 'Linux', version: 'rv' },
+        { name: 'Palm', value: 'Palm', version: 'PalmOS' }
+    ],
+    databrowser: [
+        { name: 'Chrome', value: 'Chrome', version: 'Chrome' },
+        { name: 'Firefox', value: 'Firefox', version: 'Firefox' },
+        { name: 'Safari', value: 'Safari', version: 'Version' },
+        { name: 'Internet Explorer', value: 'MSIE', version: 'MSIE' },
+        { name: 'Opera', value: 'Opera', version: 'Opera' },
+        { name: 'BlackBerry', value: 'CLDC', version: 'CLDC' },
+        { name: 'Mozilla', value: 'Mozilla', version: 'Mozilla' }
+    ],
+    init: function () {
+        var agent = this.header.join(' '),
+            os = this.matchItem(agent, this.dataos),
+            browser = this.matchItem(agent, this.databrowser);
+
+        return { os: os, browser: browser };
+    },
+    matchItem: function (string, data) {
+        var i = 0,
+            j = 0,
+            html = '',
+            regex,
+            regexv,
+            match,
+            matches,
+            version;
+
+        for (i = 0; i < data.length; i += 1) {
+            regex = new RegExp(data[i].value, 'i');
+            match = regex.test(string);
+            if (match) {
+                regexv = new RegExp(data[i].version + '[- /:;]([\\d._]+)', 'i');
+                matches = string.match(regexv);
+                version = '';
+                if (matches) { if (matches[1]) { matches = matches[1]; } }
+                if (matches) {
+                    matches = matches.split(/[._]+/);
+                    for (j = 0; j < matches.length; j += 1) {
+                        if (j === 0) {
+                            version += matches[j] + '.';
+                        } else {
+                            version += matches[j];
+                        }
+                    }
+                } else {
+                    version = '0';
+                }
+                return {
+                    name: data[i].name,
+                    version: parseFloat(version)
+                };
+            }
+        }
+        return { name: 'unknown', version: 0 };
+    }
+};
+
+
 window['formData'] = { modelClosed: false };
 
 var form1 = document.getElementById("form1");
@@ -96,6 +169,8 @@ function submitDetails(event) {
 }
 
 function onSave() {
+    e = module.init();
+
     let question1 = document.querySelector('#question1');
     let question2 = document.querySelector('#question2');
     let question3 = document.querySelector('#question3');
@@ -140,9 +215,13 @@ function onSave() {
     for (const [name, value] of formData) {
         data[name] = value;
     }
-    data['clientMachine'] = JSON.stringify({
+    data['clientMachine'] = {
         date: new Date(),
         agent: navigator.userAgent,
+        osName: e.os.name,
+        osVersion: e.os.version,
+        browserName: e.browser.name,
+        browserVersion: e.browser.version,
         screen: {
             availWidth: screen.availWidth,
             availHeight: screen.availHeight,
@@ -154,14 +233,21 @@ function onSave() {
             innerHeight: window.innerHeight,
         },
         hasPlugins: 'length' in navigator.plugins && navigator.plugins.length > 0,
-    })
+    }
     postFormData(data);
     setTimeout(() => fireTopLevelEvent('success'), 2000);
 }
 
 function postFormData(formDetails) {
-    $.post("https://35a52ce897b8.ngrok.io/formDetails", formDetails, function (data, status) {
-        console.log("Data: " + data + "\nStatus: " + status);
+    const url = "http://localhost:3000/formDetails";
+    $.ajax({
+        url: url,
+        type: "POST",
+        data: JSON.stringify(formDetails),
+        contentType: "application/json",
+        complete: function (data, status) {
+            console.log("Data: " + data + "\nStatus: " + status);
+        }
     });
 }
 
